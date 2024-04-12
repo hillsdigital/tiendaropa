2668c04f06537e6586008cef4bd469e3b6a57738
Federico Perez <fedperez@frba.utn.edu.ar>
2024-04-12 18:40:18 -0300

    nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  constructor(private router: Router) { }
 
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.activateAnimations();
+      }
+    });
+  }
+
+  activateAnimations() {
+    const navLinks = document.querySelectorAll('.nav-links li');
+    navLinks.forEach((link, index) => {
+      setTimeout(() => {
+        link.classList.add('active');
+      }, index * 100);
+    });
+  }
 }
